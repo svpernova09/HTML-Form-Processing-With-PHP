4144d3996c4d2613df8005d7f8c8e6b66e56e207
Svpernova09 <halo09@gmail.com>
2013-10-11 07:33:48 -0500

    initial commit

---
+++ examples/01 - Basic HTML Form/index.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+<form name="basic_form" id="basic_form" method="POST" action="#">
+    <label for="name">Name:
+        <input type="text" name="name" id="name" value="">
+    </label>
+</form>
