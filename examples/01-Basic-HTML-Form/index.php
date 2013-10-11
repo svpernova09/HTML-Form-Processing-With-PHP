ea760f99fdba6dffb424259f160068a86b79ce89
Svpernova09 <halo09@gmail.com>
2013-10-11 09:34:01 -0500

    Screw IE >.>

--- examples/01-Basic-HTML-Form/index.php
+++ examples/01-Basic-HTML-Form/index.php
@@ -18,11 +18,6 @@ require '../../vendor/autoload.php';
         }
     </style>
     <link href="../../vendor/components/bootstrap-default/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-    <script src="../../vendor/components/bootstrap-default/js/html5shiv.js"></script>
-    <![endif]-->
 </head>
 
 <body>
